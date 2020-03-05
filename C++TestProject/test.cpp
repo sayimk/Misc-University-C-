26a9c460529a1872ba3b570ffd077064653b4410
sayimk <sayimkhan007@googlemail.com>
2020-03-05 20:46:25 +0000

    additional c++ work

---
+++ C++TestProject/test.cpp
@@ -0,0 +1,19 @@
+/*
+ * test.cpp
+ *
+ *  Created on: 11 Oct 2017
+ *      Author: sayim
+ */
+
+//#include "test.h"
+#include <iostream>
+#include <string>
+
+namespace std {
+
+int main(){
+	cout<<"hello"<<endl;
+	return 0;
+}
+
+} /* namespace std */
---
+++ C++TestProject/test.h
@@ -0,0 +1,21 @@
+/*
+ * test.h
+ *
+ *  Created on: 11 Oct 2017
+ *      Author: sayim
+ */
+
+#ifndef TEST_H_
+#define TEST_H_
+
+namespace std {
+
+class test {
+public:
+	test();
+	virtual ~test();
+};
+
+} /* namespace std */
+
+#endif /* TEST_H_ */
